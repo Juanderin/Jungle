b1dcc7010fd137e76f852324f6a01f2c5658bd5d
Juan A <avilajajuan@gmail.com>
2023-07-23 22:19:19 -0400

    small page additions

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as sessionActions from '../../store/sessionReducer'
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+
+
+
+const loginPage = () => {
+
+    const dispatch = useDispatch();
+
+    
+
+}
