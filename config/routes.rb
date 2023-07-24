308212256a3b6d789e0fa07c8636f4eecd9e3674
Juan A <avilajajuan@gmail.com>
2023-07-23 20:58:12 -0400

    front changes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
 
-  post 'api/test', to: 'application#test'
-
 
   namespace :api, defaults: {format: :json} do 
     resources :users, only: [:create]
