40609883a3cadc1864b964d9889b85f564dad8e7
Juan A <avilajajuan@gmail.com>
2023-07-25 17:58:56 -0400

    main page nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Navigation from "./components/Navigation";
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom/";
 import SignUpForm from "./components/SignupFormPage";
+import MainPage from "./components/MainPageForm";
 import './index.css';
 
 const App = () => {
@@ -15,14 +16,15 @@ const App = () => {
   return (
   <div>
     {/* <h1>Welcome to Jungle</h1> */}
-    <div id='logo'>
+    {/* <div id='logo'>
     <img src='Jungle-7-24-2023.png'></img>
-    </div>
+    </div> */}
 
    <Switch>
    {/* <Route path='/' component={LoginPage}/> */}
     <Route exact path='/' component={Navigation}/>
     <Route path='/login' component={LoginPage}/>
+    <Route path='/main' component={MainPage} />
     <Route path='/signup' component={SignUpForm}/>
     </Switch> 
   </div>
