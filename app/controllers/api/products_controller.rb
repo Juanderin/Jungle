4431fb4711d4ba38490c498d3d203d11ad6f45ce
Juan A <avilajajuan@gmail.com>
2023-07-26 16:00:01 -0400

    product build

---
+++ app/controllers/api/products_controller.rb
@@ -0,0 +1,21 @@
+class Api::ProductsController < ApplicationController    
+
+
+    def show 
+
+        @product = Product.find_by(id: params[:id])
+        render :show
+    
+    end 
+
+
+    def index 
+
+        @products = Product.all
+        render :index
+
+    end 
+
+    
+
+end 
