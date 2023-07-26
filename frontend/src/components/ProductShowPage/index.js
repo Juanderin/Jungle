a75d0230758499c1dfbe6df7d26d9b923b696684
Juan A <avilajajuan@gmail.com>
2023-07-26 17:41:52 -0400

    pulls

---
+++ frontend/src/components/ProductShowPage/index.js
@@ -0,0 +1,30 @@
+// import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom/";
+import { fetchProduct } from "../../store/products";
+
+const ProductShow = () => {
+    const dispatch = useDispatch();
+    const productId = useParams().productId
+    const product = useSelector(state => state.products?.[productId])
+
+    useEffect(() => {
+        dispatch(fetchProduct(productId))
+    }, [productId])    
+
+
+    if (!product) return null
+
+    return(
+        <>
+        {product.productName}
+        </>
+    )
+
+}
+
+
+export default ProductShow;
+
+//
