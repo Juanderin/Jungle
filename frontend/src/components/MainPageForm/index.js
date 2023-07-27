97321e1afd6a2e18ef3a1bbb68a7ad3653676976
Juan A <avilajajuan@gmail.com>
2023-07-27 12:24:22 -0400

    small styling main

--- frontend/src/components/MainPageForm/index.js
+++ frontend/src/components/MainPageForm/index.js
@@ -12,17 +12,25 @@ const MainPage = () => {
         
         <div id='mainPageBox'>
 
-        {/* <div id='mainLogoBox'> */}
             <img id='mainLogo' src='Jungle-7-25-2023.png'/>
-            <input id='searchBox' type='text' />
-          
+                <div id='searchBoxContainer'>
+                    <input id='searchBox' type='text' placeholder="Search The Jungle"/>
+                    <button id='searchButton'>
+                        <div>
+                        <i className="fa-solid fa-magnifying-glass">
+                            Mag
+                        </i> 
+                        </div>
+                        </button>
+                </div>
+
                 <button>
                     Hello, sign in
                    <br></br>
                     <span id="AccountLists">Account & Lists</span>
                 </button>
            
-        {/* </div> */}
+    
         </div>
         
         </>
