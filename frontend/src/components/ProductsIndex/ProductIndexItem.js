111e752a587ee20158aec2dd616d38d6e79b9b3b
Juan A <avilajajuan@gmail.com>
2023-07-31 00:07:52 -0400

    price styling

--- frontend/src/components/ProductsIndex/ProductIndexItem.js
+++ frontend/src/components/ProductsIndex/ProductIndexItem.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom/";
 
 const ProductsIndexItem = ({product}) => {
+  const priceStr = product.productPrice.toFixed(2)
+  const price = priceStr.split(".")
+
 
     return (
         <>
@@ -11,6 +14,7 @@ const ProductsIndexItem = ({product}) => {
           <img id='productImg' src={product.photoUrl} alt='product_image' />
           <br></br>
           <div id='productNameOnIndex'>{product.productName}</div>
+          <div id='productPriceOnIndex'>{price[0]}<div id="productCentsOnIndex">{price[1]}</div></div>
         </Link> 
         </div>
         </>
