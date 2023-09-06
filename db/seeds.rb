7d141aacb696a7194705c2d423f8fc32358d53be
Juan A <avilajajuan@gmail.com>
2023-09-06 12:21:38 -0400

    db changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,6 +29,8 @@ ApplicationRecord.transaction do
     puts "Creating users..."
     # Create one user with an easy to remember username, email, and password:
     
+    Cart.destroy_all
+    
     User.destroy_all
 
     User.create!(
